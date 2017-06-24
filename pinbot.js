dcf454597920393aea6aa2419737240a60f43515
Jeremiah Buddenhagen <bitspill+git@bitspill.net>
2017-06-23 22:21:54 -0500

    Populate table with ipfs file sizes and hashes

--- pinbot.js
+++ pinbot.js
@@ -1,9 +1,10 @@
 let config = require('config');
+let db = require('sqlite');
 let Log = require('log');
 let fs = require('fs');
 let fsp = require('filesize-parser');
+var ipfsAPI = require('ipfs-api');
 let request = require('request');
-let db = require('sqlite');
 
 function getConfig(key, defaultVal) {
     if (config.has(key)) {
@@ -13,14 +14,6 @@ function getConfig(key, defaultVal) {
     return defaultVal;
 }
 
-
-let log = new Log(getConfig('logging.level', 'debug'),
-    fs.createWriteStream(getConfig('logging.path', './pinbot.log'), {flags: 'a'}));
-
-let diskUsageLimit = fsp(getConfig("resourceLimits.disk", "10GB"));
-
-
-let libraryMediaURL = getConfig('libraryD.url', 'https://api.alexandria.io/alexandria/v2/media/get/all');
 function refreshPublishedMedia() {
     log.info('Fetching media items');
     request(libraryMediaURL, function (err, resp, body) {
@@ -32,6 +25,7 @@ function refreshPublishedMedia() {
                     processMediaFiles(media[k]);
                 }
             }
+            console.log("meh")
         }
     });
 }
@@ -133,17 +127,75 @@ function validMultihash(string) {
     return false;
 }
 
+function refreshPinCounts() {
+
+}
+
+function updateFileSizes() {
+    db.all('SELECT * FROM pinTracker WHERE bytes IS NULL;', {Promise})
+        .then(function (rows) {
+            log.info("Updating file sizes for %d items.", rows.length);
+            let i = 1;
+            for (let row of rows) {
+                setTimeout(function() {
+                    updateFileSize(row);
+                },i++*500);
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
+
+function updateFileSize(row) {
+    if (row.fileID !== row.ipfsAddress)
+        ipfs.ls(row.ipfsAddress)
+            .then(function (res) {
+                for (let obj of res.Objects)
+                    for (let link of obj.Links) {
+                        if (obj.Hash + '/' + link.Name === row.fileID)
+                            db.run("UPDATE pinTracker SET bytes = $bytes, fileAddress = $fileAddress WHERE fileID = $fileID", {
+                                $fileID: obj.Hash + '/' + link.Name,
+                                $fileAddress: link.Hash,
+                                $bytes: link.Size
+                            });
+                        log.debug(obj.Hash + '/' + link.Name, link.Size)
+                    }
+            })
+            .catch(function (err) {
+                console.log(err)
+            });
+}
 
 function main_hehe() {
     refreshPublishedMedia();
+    setTimeout(updateFileSizes, 1.5 * 1000);
+    setTimeout(refreshPinCounts, 1.5 * 1000);
 }
+
+
+let log = new Log(getConfig('logging.level', 'debug'),
+    fs.createWriteStream(getConfig('logging.path', './pinbot.log'), {flags: 'a'}));
+let diskUsageLimit = fsp(getConfig("resourceLimits.disk", "10GB"));
+let libraryMediaURL = getConfig('libraryD.url', 'https://api.alexandria.io/alexandria/v2/media/get/all');
+let ipfs = ipfsAPI(getConfig('IPFSNode', {host: 'localhost', port: '5001', protocol: 'http'}));
 
 
 // Don't judge, I'm learning Promises as I go here and this will 100% be getting redone
 Promise.resolve()
     .then(() => db.open(getConfig("database.db", ":memory:"), {Promise}))
-    .then(() => db.migrate({force: 'last'}))
+    .then(() => db.migrate())
     .then(() => db.prepare("INSERT OR IGNORE INTO main.pinTracker (fileID, ipfsAddress) VALUES (?, ?);")
         .then((stmt) => stmtAddFileToDB = stmt))
     .then(() => main_hehe())
     .catch(err => console.error(err.stack));
+
+
+// ipfs.ls('QmWS344nttYkMJo1ooHejcuEEdZauu4T1mfJmpatssmXP9')
+//     .then(function (res) {
+//         console.log('my res is: ');
+//         console.log(res);
+//     })
+//     .catch(function (err) {
+//         console.log('Fail: ', err)
+//     });
